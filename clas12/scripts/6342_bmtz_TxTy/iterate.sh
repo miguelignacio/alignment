dc30428b01ef891a3b467b9c2ff0066078dc4a12
Sebouh Paul <sebouh.paul@gmail.com>
2021-08-23 13:17:27 -0400

    updated scripts for 6342_bmtz_TxTy

--- clas12/scripts/6342_bmtz_TxTy/iterate.sh
+++ clas12/scripts/6342_bmtz_TxTy/iterate.sh
@@ -1,5 +1,6 @@
-maxiter=16
+maxiter=6
 initial_var=rga_fall2018_svtsurvey
+config=TxTy
 #maxiter=16
 #initial_var=dev4
 
@@ -23,15 +24,17 @@ do
     ccdb mkvar dev${i} -p ${prev}
     #BMT
     ccdb dump /geometry/cvt/mvt/alignment -v $prev -r 11 | grep -v '#' > prev.txt
-    ./TableUtil --in=${result_file} --old=prev.txt --new=new.txt --config=TxTy --detector=BMT
+    ./TableUtil --in=${result_file} --old=prev.txt --new=new.txt --config=$config --detector=BMT
     ccdb add /geometry/cvt/mvt/alignment new.txt -v dev${i} -r 11-11
     cp new.txt ${thisdir}/dev${i}_bmt.txt
     #SVT
     ccdb dump /geometry/cvt/svt/layeralignment -v $prev -r 11 | grep -v '#' > prev.txt
-    ./TableUtil --in=${result_file} --old=prev.txt --new=new.txt --config=TxTy --mergeTB --detector=SVT
+    ./TableUtil --in=${result_file} --old=prev.txt --new=new.txt --config=$config --mergeTB --detector=SVT
     ccdb add /geometry/cvt/svt/layeralignment new.txt -v dev${i} -r 11-11
     cp new.txt ${thisdir}/dev${i}_svt.txt
     #set the variation for the next iteration
     cd ${thisdir}; ./changeVariation.sh align.yaml dev${i}
+
+    say "done with iteration" ${i}
     prev=dev${i}
 done 
