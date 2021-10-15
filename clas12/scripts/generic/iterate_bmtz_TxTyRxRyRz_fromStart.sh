c0808660941d87193207c6f6e6af373441ba6451
Sebouh Paul <sebouh.paul@gmail.com>
2021-10-15 17:11:55 -0400

    added iterate_bmtz_TxTyRxRyRz_fromStart.sh, and README for the generic iterate.sh

---
+++ clas12/scripts/generic/iterate_bmtz_TxTyRxRyRz_fromStart.sh
@@ -0,0 +1,8 @@
+export maxiter=6
+export initial_var=rga_fall2018_svtsurvey
+export config=TxTyRxRyRz
+export n=100000
+export align_cfg=align_bmtz_TxTyRxRyRz.cfg
+export input_file_zf=/Users/spaul/alignment/clas12/scripts/misc/filtered.hipo
+export input_file_cosmics=/Users/spaul/alignment/clas12/scripts/misc/filtered_cosmics.hipo
+./iterate.sh
