f6e0bef2a99fb241413c5925a2324490a08ab314
Sebouh Paul <sebouh.paul@gmail.com>
2021-12-07 16:24:54 -0500

    iterate_plots_for_paper.sh:  use HOME environmental variable in input file locations

--- clas12/scripts/generic/iterate_plots_for_paper.sh
+++ clas12/scripts/generic/iterate_plots_for_paper.sh
@@ -1,4 +1,4 @@
-export CCDB_CONNECTION=sqlite:////home/sebouh/input_for_alignment/clas12_cvtalign_maxime_05142020_6342.sqlite
+export CCDB_CONNECTION=sqlite:///$HOME/input_for_alignment/clas12_cvtalign_maxime_05142020_6342.sqlite
 export recompile=false
 export maxiter=8
 export initial_var=rga_fall2018_svtsurvey
@@ -7,6 +7,6 @@ export config=TxTyTzRxRyRz
 export n_zf=30000
 export n_cosmics=30000
 export align_cfg=align_all.cfg
-export input_file_zf=/home/sebouh/input_for_alignment/fieldoff_6342.hipo
-export input_file_cosmics=/home/sebouh/input_for_alignment/cosmic_6117.hipo
+export input_file_zf=$HOME/input_for_alignment/fieldoff_6342.hipo
+export input_file_cosmics=$HOME/input_for_alignment/cosmic_6117.hipo
 ./iterate.sh
