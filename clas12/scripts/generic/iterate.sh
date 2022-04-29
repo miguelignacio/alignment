d8096c586a2b3f6149ea3cc9b4ada114622302c5
sebouh137 <sebouh.paul@gmail.com>
2022-04-29 09:42:09 -0700

    added more conditional cases to iterate.sh

--- clas12/scripts/generic/iterate.sh
+++ clas12/scripts/generic/iterate.sh
@@ -8,6 +8,13 @@ fi
 #add user to list of users in the CCDB
 if [[ `ccdb user --list` != *${CCDB_USER}* ]]; then
     ccdb user --create $CCDB_USER
+    echo "iterate.sh automatically adding user "$CCDB_USER" to the ccdb sqlite"
+fi
+
+#check if CCDB_CONNECTION environmental variable is set.  TODO: add more statements like this.
+if [[ -z "${CCDB_CONNECTION}"]]; then
+    echo "error:  Env. variable CCDB_CONNECTION is not defined. Exiting"
+    exit 0
 fi
 
 
